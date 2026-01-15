b5ec97392b4e442d8cdc597c9e85de0874657414
yeahgor <yeahgorq@gmail.com>
2026-01-15 09:35:12 +0300

    feat: реализована инициализация и запуск всех слоев приложения

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,38 @@
-package app
+package main
+
+import (
+	"context"
+	"log"
+
+	"RedCollar/internal/config"
+	v1 "RedCollar/internal/delivery/http/v1"
+	"RedCollar/internal/repository"
+	"RedCollar/internal/service"
+)
+
+func main() {
+	ctx := context.Background()
+	cfg, err := config.GetEnv()
+	if err != nil {
+		log.Fatalf("не удалось получить данные из .env: %v", err)
+	}
+
+	db, err := repository.NewPostgresConnection(ctx, cfg)
+	if err != nil {
+		log.Fatal("не удалось подключиться к БД(postgres):", err)
+	}
+
+	rdb, err := repository.RedisConnection(ctx, cfg.RedisAddr)
+	if err != nil {
+		log.Fatal("не удалось подключиться к БД(redis):", err)
+	}
+
+	serv := service.NewIncidentService(db, rdb, cfg.WarningZone, cfg.CacheTTL)
+
+	h := v1.NewHandler(serv, cfg.StatsTime)
+
+	log.Printf("сервер запущен на порту: %s", cfg.AppPort)
+	if err := h.Run(cfg.AppPort); err != nil {
+		log.Fatal("ошибка запуска сервера:", err)
+	}
+}
