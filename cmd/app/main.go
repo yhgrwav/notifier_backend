aed39e8408313ab43b178c388a0eeca47071b142
yeahgor <yeahgorq@gmail.com>
2026-01-17 13:40:50 +0300

    feat: добавлены комментарии, API ключ, корректный вызов воркера в горутине и контекст для graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"RedCollar/internal/worker"
 	"context"
 	"log"
+	"os/signal"
+	"syscall"
 
 	"RedCollar/internal/config"
 	v1 "RedCollar/internal/delivery/http/v1"
@@ -11,35 +14,50 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
+	//создаём контекст для управления программой
+	//при нажатии ctrl+c отправляем сигнал и отключаемся
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	//читаем и записываем переменные из .env в переменную cfg
 	cfg, err := config.GetEnv()
 	if err != nil {
 		log.Fatalf("не удалось получить данные из .env: %v", err)
 	}
 
+	//устанавливаем подключение с PostgreSQL
 	db, err := repository.NewPostgresConnection(ctx, cfg)
 	if err != nil {
 		log.Fatal("не удалось подключиться к БД(postgres):", err)
 	}
 
+	//устанавливаем подключение с Redis
 	rdb, err := repository.RedisConnection(ctx, cfg.RedisAddr)
 	if err != nil {
 		log.Fatal("не удалось подключиться к БД(redis):", err)
 	}
 
+	//инициализируем сервис
 	serv := service.NewIncidentService(db, rdb, cfg.WarningZone, cfg.CacheTTL)
 
+	//инициализируем HTTP клиента
 	client := service.NewHTTPClient(cfg.WebhookTimeout)
 
+	//инициализируем воркера
+	w := worker.NewWebhookWorker(rdb, client, cfg.WebhookUrl, cfg.WebhookRetries)
+
+	//запускаем в воркера в горутине чтобы не блокировать основное выполнение программы
 	go func() {
-		log.Printf("вебхук воркер запущен по адресу %s", cfg.WebhookUrl)
-		worker.NewWebhookWorker(rdb, client, cfg.WebhookUrl, cfg.WebhookRetries)
+		w.Run(ctx)
 	}()
 
+	//инициализируем сервер
 	h := v1.NewHandler(serv, cfg.StatsTime)
 
 	log.Printf("сервер запущен на порту: %s", cfg.AppPort)
-	if err := h.Run(cfg.AppPort); err != nil {
+
+	//запускаем сервер
+	if err := h.Run(cfg.AppPort, cfg.ApiKey); err != nil {
 		log.Fatal("ошибка запуска сервера:", err)
 	}
 }
